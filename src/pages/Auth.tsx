a90f738b335a1df30f71546539e768dc5644ace6
best200-lab <67961530+best200-lab@users.noreply.github.com>
2025-09-21 10:21:08 +0100

    Update Auth.tsx
    update "https://chat.juristmind.com/" url

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const Auth = () => {
   useEffect(() => {
     // redirect to frontend's login during development
-    window.location.href = "http://localhost:8080/";
+    window.location.href = "https://chat.juristmind.com/";
   }, []);
 
   return null;
